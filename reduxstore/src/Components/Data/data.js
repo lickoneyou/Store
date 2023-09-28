64494baeffe2769acdf84c5ba5bd73aff36ab0a2
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-09-28 18:18:40 +0300

    add laptops data

---
+++ reduxstore/src/Components/Data/data.js
@@ -0,0 +1,72 @@
+export const data = {
+  laptops: [
+    {
+      id: 1,
+      name: 'Macbook Air 13',
+      price: 500,
+      description:
+        '13.3" 2560 x 1600, IPS, 60 Гц, Apple M1, 8 ГБ, SSD 256 ГБ, видеокарта встроенная, Mac OS, цвет крышки серый, аккумулятор 49.9 Вт·ч',
+      frontPic: './assets/goods/laptops/MacbookAir13/front.jpeg',
+      otherPic: [
+        './assets/goods/laptops/MacbookAir13/1.jpeg',
+        './assets/goods/laptops/MacbookAir13/2.jpeg',
+        './assets/goods/laptops/MacbookAir13/3.jpeg',
+        './assets/goods/laptops/MacbookAir13/4.jpeg',
+      ],
+    },
+    {
+      id: 2,
+      name: 'ASUS VivoBook',
+      price: 300,
+      description:
+        '15.6" 1920 x 1080, IPS, 60 Гц, Intel Core i5 1135G7, 8 ГБ DDR4, SSD 256 ГБ, видеокарта встроенная, Linux, цвет крышки темно-серый, аккумулятор 42 Вт·ч',
+      frontPic: './assets/goods/laptops/ASUSVivoBook/front.jpeg',
+      otherPic: [
+        './assets/goods/laptops/ASUSVivoBook/1.jpeg',
+        './assets/goods/laptops/ASUSVivoBook/2.jpeg',
+        './assets/goods/laptops/ASUSVivoBook/3.jpeg',
+        './assets/goods/laptops/ASUSVivoBook/4.jpeg',
+      ],
+    },
+    {
+      id: 3,
+      name: 'HP 255',
+      price: 250,
+      description:
+        '15.6" 1920 x 1080, IPS, 60 Гц, AMD Ryzen 5 5500U, 16 ГБ DDR4, SSD 512 ГБ, видеокарта встроенная, без ОС, цвет крышки темно-серый, аккумулятор 41 Вт·ч',
+      frontPic: './assets/goods/laptops/HP255/front.jpeg',
+      otherPic: [
+        './assets/goods/laptops/HP255/1.jpeg',
+        './assets/goods/laptops/HP255/2.jpeg',
+        './assets/goods/laptops/HP255/3.jpeg',
+        './assets/goods/laptops/HP255/4.jpeg',
+      ],
+    },
+    {
+      id: 4,
+      name: 'Acer Aspire 3',
+      price: 280,
+      description:
+        '15.6" 1920 x 1080, IPS, 60 Гц, Intel Core i3 1115G4, 8 ГБ DDR4, SSD 512 ГБ, видеокарта встроенная, Windows 11, цвет крышки серебристый, аккумулятор 36.7 Вт·ч',
+      frontPic: './assets/goods/laptops/AcerAspire3/front.jpeg',
+      otherPic: [
+        './assets/goods/laptops/AcerAspire3/1.jpeg',
+        './assets/goods/laptops/AcerAspire3/2.jpeg',
+        './assets/goods/laptops/AcerAspire3/3.jpeg',
+        './assets/goods/laptops/AcerAspire3/4.jpeg',
+      ],
+    },
+    {
+      id: 5,
+      name: 'Maibenben X757',
+      price: 480,
+      description:
+        '17.3" 2560 x 1440, IPS, 165 Гц, AMD Ryzen 7 5800H, 16 ГБ DDR4, SSD 512 ГБ, видеокарта NVIDIA GeForce RTX 3070 8 ГБ (TGP 140 Вт), Linux, цвет крышки серый, аккумулятор 46.74 Вт·ч',
+      frontPic: './assets/goods/laptops/MaibenbenX757/front.jpeg',
+      otherPic: [
+        './assets/goods/laptops/MaibenbenX757/1.jpeg',
+        './assets/goods/laptops/MaibenbenX757/front.jpeg',
+      ],
+    },
+  ],
+}

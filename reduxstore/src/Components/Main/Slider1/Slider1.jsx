f32863cd520f7ddbfc0201ca8485c919945b21f7
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-09-28 16:07:49 +0300

    add slider

--- reduxstore/src/Components/Main/Slider1/Slider1.jsx
+++ reduxstore/src/Components/Main/Slider1/Slider1.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import styles from './Slider1.module.css'
 
 const Slider1 = () => {
+  var settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 5000,
+    autoplaySpeed: 2000,
+    cssEase: 'linear',
+  }
   return (
-    <div>Slider1</div>
+    <Slider className={styles.slider} {...settings}>
+      <div>
+        <img className={styles.slide} src="./assets/Slider/1.png" />
+      </div>
+      <div>
+        <img className={styles.slide} src="./assets/Slider/2.png" />
+      </div>
+      <div>
+        <img className={styles.slide} src="./assets/Slider/3.png" />
+      </div>
+    </Slider>
   )
 }
 

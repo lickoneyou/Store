7feadd6d92ecf1747c3e2abf3de1fee0b686380b
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-10-03 17:59:36 +0300

    completed footer

--- reduxstore/src/App.js
+++ reduxstore/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import './reset.css';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
     <Header />
     <Main />
+    <Footer />
     </div>
   );
 }

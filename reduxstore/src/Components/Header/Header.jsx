99bd83496303f7c218e0fa3817798c25a3fd8100
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-10-05 13:14:17 +0300

    add header cart total

--- reduxstore/src/Components/Header/Header.jsx
+++ reduxstore/src/Components/Header/Header.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import styles from './Header.module.css'
+import React from "react";
+import styles from "./Header.module.css";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const total = useSelector((state) => state.cart.price);
   return (
     <header>
-      <a href="#/"><img src="./assets/Logo.png" alt="Logo" /></a>
+      <a href="#/">
+        <img src="./assets/Logo.png" alt="Logo" />
+      </a>
       <button className={styles.katalog}>KATALOG</button>
       <div>
-        <input type='text' placeholder='Maxsulotlarni izlash'/>
+        <input type="text" placeholder="Maxsulotlarni izlash" />
         <button>
           <img src="./assets/icons/search.png" alt="search" />
         </button>
       </div>
-      <a href="#Cart"><img className={styles.cart} src="./assets/icons/Korzina.png" alt="search" /></a>
+      <div className={styles.cartWrapper}>
+        <p>
+          <b>{total > 0 ? total + '$' : ''}</b>
+        </p>
+        <a href="#Cart">
+          <img
+            className={styles.cart}
+            src="./assets/icons/Korzina.png"
+            alt="search"
+          />
+        </a>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
